5b57d500f970fef77c18c82332c23f518c329852
vietphan95 <vietphan95@outlook.com>
2026-03-26 03:14:37 +0700

    save isInSleep

---
+++ PhanMemSongAm1/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhanMemSongAm1
+{
+    public class Log
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ PhanMemSongAm1/NameDetector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PhanMemSongAm1
+{
+    internal class NameDetector
+    {
+        public NameDetector() { }
+
+        public List<string> Detector(string message) 
+        {
+            var names = new List<string>();
+            var elements = message.Split(' ');
+            for (int i =0; i< elements.Length; i++)
+            {
+                if (elements[i].FirstOrDefault() != null && char.IsUpper(elements[i].FirstOrDefault()))
+                {
+                    names.Add(elements[i]);
+                }
+            }
+
+            return names;
+        }
+    }
+}
